06295639db0d547ff76578f049b96ce62421e244
harish2k01 <t.harish2478@gmail.com>
2023-12-14 22:34:09 +0530

    Splitted the Components and Styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,35 @@
 import * as React from "react"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { FaHouse } from "react-icons/fa6"
 
-const NotFoundPage = () => (
-  <Layout>
-    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-white">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
-        <p className="mt-6 text-base leading-7 text-gray-400">Sorry, we couldn’t find the page you’re looking for.</p>
-        <a href="/" className='relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50' style={{width : "5rem", marginTop : "3rem"}}>
-          <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
-          <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl'>
-            Home
-          </span>
-        </a>
-      </div>
-    </main>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const homePage = () => {
+    window.location.href = "/"
+  }
+
+  return (
+    <Layout>
+      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-white">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
+            Page not found
+          </h1>
+          <p className="mt-6 text-base leading-7 text-gray-400">
+            Sorry, we couldn’t find the page you’re looking for!
+          </p>
+          <div className="flex items-center justify-center">
+            <button className="button" onClick={homePage}>
+              <FaHouse className="button-icon" />
+              <span>Home</span>
+            </button>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  )
+}
 
 export const Head = () => <Seo title="404: Not Found" />
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,73 +1,20 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import "../styles/global.css"
 import "../styles/index.css"
-import { FaGithub, FaLinkedin, FaMedium } from "react-icons/fa6"
+import Intro from "../components/intro"
 
-const githubPage = () => {
-  window.open("https://github.com/harish2k01", "_blank")
-}
-
-const linkedinPage = () => {
-  window.open("https://www.linkedin.com/in/harish2k01", "_blank")
-}
-
-const mediumPage = () => {
-  window.open("https://harish2k01.medium.com/", "_blank")
-}
-
-const IndexPage = () => (
-  <Layout>
-    <div>
-      <div className="status-container">
-        <div className="green-circle"></div>
-        <span>Online</span>
+const IndexPage = () => {
+  return (
+    <Layout>
+      <div>
+        <Intro />
+        <h2 className="heading">My Timeline</h2>
       </div>
-      <section className="mx-auto max-w-lg px-12 sm:px-0">
-        <article className="text-slate-200 slide-in-bottom">
-          <h1 className="text-4xl sm:text-5xl font-semibold mt-5 tracking-wider">
-            Hi, I'm <span class="marker">Harish</span>
-            <br />
-            <span
-              className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mt-2 block"
-              style={{ paddingBottom: "3px" }}
-            >
-              Software Engineer
-            </span>
-          </h1>
-          <div className="button-group">
-          <button className="button" onClick={linkedinPage}>
-              <FaLinkedin className="button-icon" />
-              <span>LinkedIn</span>
-            </button>
-            <button className="button" onClick={githubPage}>
-              <FaGithub className="button-icon" />
-              <span>GitHub</span>
-            </button>
-            <button className="button" onClick={mediumPage}>
-              <FaMedium className="button-icon" />
-              <span>Medium</span>
-            </button>
-          </div>
-          <p className="text-gray-400 text-lg tracking-wide mt-9">
-            Hey, I am Harish, Software Engineer from Madurai, with 1.5 years of
-            expertise in Automation and Performance Tests. A Tech Explorer who
-            is currently into Home Lab!
-          </p>
-          <hr className="line" />
-          <h2 className="heading">My Timeline</h2>
-        </article>
-      </section>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="Harish" />
 
 export default IndexPage
