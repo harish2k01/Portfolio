007cac819e16444833bf16bf281da183bddd1364
harish2k01 <t.harish2478@gmail.com>
2023-12-14 20:32:09 +0530

    Added Media Buttons

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,12 +10,12 @@ const NotFoundPage = () => (
         <p className="text-base font-semibold text-white">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
         <p className="mt-6 text-base leading-7 text-gray-400">Sorry, we couldn’t find the page you’re looking for.</p>
-        <button className='relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
+        <a href="/" className='relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50' style={{width : "5rem", marginTop : "3rem"}}>
           <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
           <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl'>
             Home
           </span>
-        </button>
+        </a>
       </div>
     </main>
   </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,68 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "../styles/global.css"
-import * as styles from "../styles/index.module.scss"
+import "../styles/index.css"
+import { FaEnvelope, FaGithub, FaLinkedin, FaMedium, FaRegEnvelope } from "react-icons/fa6"
+
+const githubPage = () => {
+  window.open("https://github.com/harish2k01", "_blank")
+}
+
+const linkedinPage = () => {
+  window.open("https://www.linkedin.com/in/harish2k01", "_blank")
+}
+
+const mediumPage = () => {
+  window.open("https://harish2k01.medium.com/", "_blank")
+}
+
+const sendEmail = () => {
+  const emailAddress = 't.harish2478@gmail.com';
+  const mailtoLink = `mailto:${emailAddress}`;
+  window.location.href = mailtoLink;
+};
 
 const IndexPage = () => (
   <Layout>
     <div>
+      <div className="status-container">
+        <div className="green-circle"></div>
+        <span>Online</span>
+      </div>
       <section className="mx-auto max-w-lg px-12 sm:px-0">
         <article className="text-slate-200 slide-in-bottom">
           <h1 className="text-4xl sm:text-5xl font-semibold mt-5 tracking-wider">
             Hi, I'm <span class="marker">Harish</span>
             <br />
-            <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mt-2 block" style={{paddingBottom: '3px'}}>
+            <span
+              className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mt-2 block"
+              style={{ paddingBottom: "3px" }}
+            >
               Software Engineer
             </span>
           </h1>
+          <div className="button-group">
+            <button className="button" onClick={githubPage}>
+              <FaGithub className="button-icon" />
+              <span>GitHub</span>
+            </button>
+            <button className="button" onClick={linkedinPage}>
+              <FaLinkedin className="button-icon" />
+              <span>LinkedIn</span>
+            </button>
+            <button className="button" onClick={mediumPage}>
+              <FaMedium className="button-icon" />
+              <span>Medium</span>
+            </button>
+          </div>
           <p className="text-gray-400 text-lg tracking-wide mt-9">
-            Hey, I am Harish, Software Engineer from Madurai, with 1.5
-            years of expertise in Automation and Performance Tests. A Tech Explorer 
-            who is currently into Home Lab!
+            Hey, I am Harish, Software Engineer from Madurai, with 1.5 years of
+            expertise in Automation and Performance Tests. A Tech Explorer who
+            is currently into Home Lab!
           </p>
-          <hr className="my-5 opacity-30" />
-          <h2 className="text-2xl font-semibold">My Timeline</h2>
+          <hr className="line" />
+          <h2 className="heading">My Timeline</h2>
         </article>
       </section>
     </div>
