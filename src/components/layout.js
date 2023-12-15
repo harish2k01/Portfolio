f05eff5517982e94105eac83602029aa3f2d80b5
harish2k01 <t.harish2478@gmail.com>
2023-12-15 23:05:03 +0530

    Updated Min Height

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,10 @@ const Layout = ({ children }) => {
     <>
       <div
         className="absolute top-0 z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#000_1px)] bg-[size:30px_30px]"
-        style={{ minWidth: "-webkit-fill-available", minHeight: "-webkit-fill-available" }}
+        style={{
+          minWidth: "-webkit-fill-available",
+          minHeight: "-webkit-fill-available",
+        }}
       >
         <div
           className="flex items-center justify-center"
